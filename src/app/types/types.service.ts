d5cbce8d0c04b4b03d3c204617adad9a1c4a68e5
Jacky Lee <jackylee@live.at>
2022-09-16 18:27:15 +0200

    began the logic to create fields

--- src/app/types/types.service.ts
+++ src/app/types/types.service.ts
@@ -1,5 +1,63 @@
 export type Field = {
-  danger: boolean
-  reward: string
+  index: number,
+  danger?: Danger,
+  reward?: Character | Item,
+  visited: boolean
 }
+
+export type Character = {
+  level: number,
+  health: number,
+  defense: number,
+  attack: number,
+  agility: number,
+  critical: number,
+  mana: number,
+  wisdom: number,
+  spells: [Spell?, Spell?, Spell?],
+  currentField?: number,
+  nextField?: number,
+  item: Item
+}
+
+type Item = {
+  breastplate: Breastplate,
+  weapon: Weapon,
+  boots: Boots,
+  magicItem: MagicItem
+}
+
+type Breastplate = {
+  health: number,
+  defense: number
+}
+
+type Weapon = {
+  attack: number,
+  critical: number
+}
+
+type Boots = {
+  agility: number,
+  critical: number
+}
+
+type MagicItem = {
+  mana: number,
+  wisdom: number
+}
+
+type Spell = {
+  name: string,
+  damage: number,
+  heal: number,
+  mana: number
+}
+
+type Danger = {
+  enemy?: Character,
+  trap?: number
+}
+
+
 
